37e2171645f96dd12a0dd33ca7fb55ad419bb12b
Jiri Studnicka <studnicka.jiri@gmail.com>
2012-10-05 14:41:29 +0200

    test

--- hello/Program.cs
+++ hello/Program.cs
@@ -16,6 +16,7 @@ namespace hello
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
         }
     }
 }
